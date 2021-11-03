b5690b382135ad450c9f10246c01b12aa63349ae
elias99moguel@gmail.com <elias99moguel@gmail.com>
2021-11-02 22:36:55 -0600

    error de prueba

--- tests/agregarTest.php
+++ tests/agregarTest.php
@@ -9,7 +9,7 @@ class CrudTest extends \PHPUnit\Framework\TestCase
 
 	public function testAdd(){
 		include "conexion.php";
-		$nombre = "Andrea";
+		//$nombre = "Andrea";
 		$fecha = "02/11/2021";
 		$correo = "andreaturriza@gmail.com";
 		 $sentencia = $base_de_datos->prepare("INSERT INTO empresa (nombre, correo, fecha_i) VALUES  (?, ?, ?);");
